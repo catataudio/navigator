90eb014fdad57c519ead3925980b76594bf44dc2
Frederick Arcinegas <125893494+catataudio@users.noreply.github.com>
2025-08-27 15:25:30 -0400

    Add model definitions for Scene, Performer, and FileReference

--- navigator/navigatorApp.swift
+++ navigator/navigatorApp.swift
@@ -13,6 +13,9 @@ struct navigatorApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
             Item.self,
+            Scene.self,
+            Performer.self,
+            FileReference.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
